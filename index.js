c25aae470dee19a5ec184f8760ba585905ad4144
KaeTheDev <kaethedev@gmail.com>
2025-10-27 12:04:50 -0400

    Added function that prints lastName, firstName

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Task 1: Flexible String Manipulation with Functions
 
+function formatFullName(lastName, firstName) {
+    return lastName + ", " + firstName
+}
+
+formatFullName("Reid", "Shakira")
+
 // Task 2: Mathematical Operations with Multiple Parameters
+
+
 
 // Task 3: Functions with Conditional Logic
 
