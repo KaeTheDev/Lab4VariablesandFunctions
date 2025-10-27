78b687c3cbba76fa5113db41471d0c31182480b7
KaeTheDev <kaethedev@gmail.com>
2025-10-27 11:37:40 -0400

    Initial project set up

---
+++ index.js
@@ -0,0 +1,7 @@
+// Task 1: Flexible String Manipulation with Functions
+
+// Task 2: Mathematical Operations with Multiple Parameters
+
+// Task 3: Functions with Conditional Logic
+
+// Task 4: Refactoring for Reusability
